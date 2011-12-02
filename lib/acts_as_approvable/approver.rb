5650afc8faa521a975af9ac385985194bf28ea84
Jay Hayes <ur@iamvery.com>
2011-12-02 16:49:16 -0600

    Added options; Better scope; Post create and update callbacks
    Can configure to return default value when approval is missing. This is
    also the value used as the default for the approval when it is first
    created.
    
    Also specify a value to set the approval after the record is updated.
    
    Wrapped scope in lambda so that SQL calls don't go out during init.
    
    Set the approval association to destroy approvals if the record is removed.

--- lib/acts_as_approvable/approver.rb
+++ lib/acts_as_approvable/approver.rb
@@ -8,39 +8,52 @@ module ActsAsApprovable
     
     module ClassMethods
       # Creates associations with +Approvals+ and add instance methods for getting approval status
-      def acts_as_approvable
+      def acts_as_approvable(options = {})
         # don't allow multiple calls
         return if included_modules.include?(ActsAsApprovable::Approver::InstanceMethods)
         
+        # default approval states when there is no approval and after the record is updated
+        options[:when_missing] ||= false
+        options[:post_update] ||= false
+        
+        class_attribute :when_missing
+        self.when_missing = options[:when_missing]
+        
+        class_attribute :post_update
+        self.post_update = options[:post_update]
+        
         # association with approval
-        has_one :approval, :as => :approvable
+        has_one :approval, :as => :approvable, :dependent => :destroy
         
         # access to all models that have been approved
-        scope :approved, joins(:approval).where(Approval.arel_table[:approved].eq(true))
+        scope :approved, lambda{ joins(:approval).where(Approval.arel_table[:approved].eq(true)) }
         
-        # make sure every approvable model has an associated approval
-        after_create :create_pending_approval
+        # make sure every new approvable model has an associated approval
+        after_create :create_approval
+        after_update :update_approval
         
         include ActsAsApprovable::Approver::InstanceMethods
       end
     end
     
     module InstanceMethods
+      # Returns whether the approval is approved. Defaults to +default_approval+ if the approval is missing
       def approved?
-        !!approval.try(:approved?)
+        approval.nil? ? when_missing : approval.try(:approved?)
       end
       
+      # Return whether the the record is not approved
       def pending?
         !approved?
       end
       
       def approve!(who = nil)
-        create_pending_approval if approval.nil?
+        create_approval if approval.nil?
         approval.update_attributes :approved => true, :approver => who || nil if pending?
       end
       
       def disapprove!(who = nil)
-        create_pending_approval if approval.nil?
+        create_approval if approval.nil?
         approval.update_attributes :approved => false, :approver => who || nil if approved?
       end
       
@@ -50,8 +63,13 @@ module ActsAsApprovable
       
       private
       
-      def create_pending_approval
-        self.approval = Approval.create :approvable => self
+      def create_approval
+        self.approval = Approval.create :approvable => self, :approved => when_missing
+      end
+      
+      def update_approval
+        create_approval if approval.nil?
+        self.approval.update_attributes :approved => post_update
       end
     end
   end
